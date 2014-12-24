2d0a35cc92f39b0fbd3cabcb7911be17beea3a7d
Putilov Michael <post.snowy@gmail.com>
2014-12-24 23:47:34 +0500

    test1

---
+++ test/base/test1.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+from twisted.internet.endpoints import clientFromString
+from twisted.internet.protocol import ClientFactory
+
+from twisted.protocols.amp import AMP
+
+from test.base.baseTest import PongLocator
+
+
+__author__ = 'snowy'
+from twisted.internet import defer, protocol
+from twisted.trial import unittest
+
+
+class ServerProtocolStub(AMP):
+    def connectionLost(self, reason):
+        super(ServerProtocolStub, self).connectionLost(reason)
+        self.factory.onConnectionLost.callback(self)
+
+
+class ClientProtocolStub(AMP):
+    def connectionMade(self):
+        super(ClientProtocolStub, self).connectionMade()
+        # self.factory.onConnectionMade.callback(self)
+
+    def connectionLost(self, reason):
+        super(ClientProtocolStub, self).connectionLost(reason)
+        self.factory.onConnectionLost.callback(self)
+
+
+class TestDisconnect(unittest.TestCase):
+    def save(self, key, value):
+        setattr(self, key, value)
+        return value
+
+    def setUp(self):
+        from twisted.internet import reactor
+
+        self.reactor = reactor
+        """:type reactor: twisted.internet.selectreactor.SelectReactor"""
+
+        self.serverDisconnected = defer.Deferred()
+        self.serverPort = self._listenServer(self.serverDisconnected)
+        self.clientDisconnected = defer.Deferred()
+        return self._connectClient(self.clientDisconnected).addCallback(lambda proto: self.save("clientProtocol", proto))
+
+    def _listenServer(self, d):
+        """
+        Начать слушать порт на сервере
+        :param d: defer.Deferred который вызывается при connectionLost
+        :return: defer.Deferred с результатом Port
+        """
+
+        f = protocol.Factory()
+        f.onConnectionLost = d
+        f.protocol = lambda: ServerProtocolStub(locator=PongLocator())
+        return self.reactor.listenTCP(0, f)
+
+    def _connectClient(self, d2):
+        """
+        Подключиться к серверу
+        :param d2: defer.Deferred вызываемый при connectionLost
+        :return: defer.Deferred с результатом подключения (инициализированный протокол ClientProtocolStub)
+        """
+        factory = ClientFactory.forProtocol(ClientProtocolStub)
+        factory.onConnectionLost = d2
+        return clientFromString(self.reactor, b"tcp:host=localhost:port={0}".format(self.serverPort.getHost().port)) \
+            .connect(factory)
+
+    def tearDown(self):
+        d = defer.maybeDeferred(self.serverPort.stopListening)
+        self.clientProtocol.transport.loseConnection()
+        return defer.gatherResults([d,
+                                    self.clientDisconnected,
+                                    self.serverDisconnected])
+
+    def test_disconnect(self):
+        pass
+
+
